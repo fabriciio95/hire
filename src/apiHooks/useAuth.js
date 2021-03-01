cf18b38e390fea3cbed18a6ac9ddad575e902a07
fabriciio95 <fabriciusiqueira@gmail.com>
2021-03-01 16:50:22 -0300

    Inicio da migração para o react hooks

---
+++ src/apiHooks/useAuth.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { JWT_TOKEN_NAME } from "../constants";
+
+class AuthService {
+
+  login(usuario, senha, onLogin) {
+      axios.post(`http://localhost:8080/login`, { usuario : usuario, senha : senha })
+           .then(response =>  {
+             const jwtToken = response.headers['authorization'].replace("Bearer ", "");
+             sessionStorage.setItem(JWT_TOKEN_NAME, jwtToken);
+             onLogin(true);
+           }).catch((error)  => {
+             console.log(error);
+             onLogin(false);
+            });
+  }
+
+  getJWTToken(){ 
+    return sessionStorage.getItem(JWT_TOKEN_NAME);
+  }
+  
+
+  isAuthenticated(){
+    return sessionStorage.getItem(JWT_TOKEN_NAME) != null;
+  }
+
+  logout(){
+    sessionStorage.removeItem(JWT_TOKEN_NAME);
+  }
+
+  getJWTTokenData() {
+    const jwtToken = this.getJWTToken();
+    if(jwtToken == null) {
+      return null;
+    }
+
+    const jwtTokenData = atob(jwtToken.split(".")[1]);
+    return JSON.parse(jwtTokenData);
+  }
+}
+
+export default new AuthService();
