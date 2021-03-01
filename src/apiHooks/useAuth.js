1aef4c2db9bc4984bf36ec0dd6f2ce72e71927ce
fabriciio95 <fabriciusiqueira@gmail.com>
2021-03-01 17:26:34 -0300

    Criando o hook de autenticação

--- src/apiHooks/useAuth.js
+++ src/apiHooks/useAuth.js
@@ -1,42 +1,53 @@
 import axios from "axios";
-import { JWT_TOKEN_NAME } from "../constants";
+import { useEffect, useState } from "react";
+import { CREDENTIALS_NAME, JWT_TOKEN_NAME } from "../constants";
 
-class AuthService {
+export const useAuth = () => {
+  const [credentials, setCredentials] = useState({ id : null, token : null });
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState(false);
 
-  login(usuario, senha, onLogin) {
-      axios.post(`http://localhost:8080/login`, { usuario : usuario, senha : senha })
-           .then(response =>  {
-             const jwtToken = response.headers['authorization'].replace("Bearer ", "");
-             sessionStorage.setItem(JWT_TOKEN_NAME, jwtToken);
-             onLogin(true);
-           }).catch((error)  => {
-             console.log(error);
-             onLogin(false);
-            });
-  }
+  useEffect(() => {
+    loadCredentials();
+  }, []);
 
-  getJWTToken(){ 
-    return sessionStorage.getItem(JWT_TOKEN_NAME);
-  }
-  
-
-  isAuthenticated(){
-    return sessionStorage.getItem(JWT_TOKEN_NAME) != null;
-  }
-
-  logout(){
-    sessionStorage.removeItem(JWT_TOKEN_NAME);
-  }
-
-  getJWTTokenData() {
-    const jwtToken = this.getJWTToken();
-    if(jwtToken == null) {
-      return null;
+  const login = async (usuario, senha) => {
+    const dataUser = { usuario : usuario, senha : senha };
+    setProcessing(true);
+    try {
+      const response = await axios.post(`http://localhost:8080/login`, dataUser);
+      const token = response.headers['authorization'].replace("Bearer ", "");
+      storeCredentials(token);
+      setProcessing(false);
+    } catch(error) {
+      setError("Usuário ou senha inválidos!");
+      setProcessing(false);
     }
 
-    const jwtTokenData = atob(jwtToken.split(".")[1]);
-    return JSON.parse(jwtTokenData);
   }
-}
 
-export default new AuthService();
+ const storeCredentials = (token) => {
+   const tokenData = atob(token.split(".")[1]);
+   const credentials = { id : tokenData.userId, token : token };
+   sessionStorage.setItem(CREDENTIALS_NAME, JSON.stringify(credentials));
+   setCredentials(credentials);
+ }
+
+ const loadCredentials = () => {
+   const storedCredentials = sessionStorage.getItem(CREDENTIALS_NAME);
+   if(storedCredentials !== null) {
+      setCredentials(JSON.parse(storedCredentials));
+   }
+ }
+
+ const logout = () => {
+  sessionStorage.removeItem(CREDENTIALS_NAME);
+  setCredentials({ id : null, token : null });
+ }
+
+ const isAuthenticated = () => {
+   return sessionStorage.getItem(CREDENTIALS_NAME) !== null;
+ }
+
+ return { login, logout, isAuthenticated, credentials, error, processing };
+}
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ const SERVER = process.env.REACT_APP_SERVER;
 
 export const JWT_TOKEN_NAME = "jwt";
 export const ENDPOINT = `${SERVER}`
+export const CREDENTIALS_NAME = "credentials";
