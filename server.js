d277f432012564c53bfa9dd3763b9e45854adaf2
fabriciio95 <fabriciusiqueira@gmail.com>
2021-03-31 21:21:27 -0300

    Config express server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("batata");
+const express = require('express');
+
+const app = express();
+
+const { resolve, dirname } = require('path')
+
+app.use('/', express.static(
+  resolve(
+    --dirname,
+    './build'
+  )
+))
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if(err) { return console.log(err)}
+
+  console.log("Tudo funcionando certinho!")
+})
